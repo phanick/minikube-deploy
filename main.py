0aa8ec6c5a6949cf989971685cb72496f589b3f8
phanick <39258517+phanick@users.noreply.github.com>
2022-07-29 19:46:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Python app deployment on Minikube cluster, running!"
+    return "Python app running on Minikube cluster!!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
